29aa216d4967fdd1f199d462927571f4728638ac
920701777 <920701777@qq.com>
2018-01-04 18:45:23 +0800

    linhao

---
+++ application/user/validate/MyInfo.php
@@ -0,0 +1,69 @@
+<?php
+// +----------------------------------------------------------------------
+// | 海豚PHP框架 [ DolphinPHP ]
+// +----------------------------------------------------------------------
+// | 版权所有 2016~2017 河源市卓锐科技有限公司 [ http://www.zrthink.com ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://dolphinphp.com
+// +----------------------------------------------------------------------
+// | 开源协议 ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+
+namespace app\user\validate;
+
+use think\Validate;
+/**
+ * 用户验证器
+ * @package app\admin\validate
+ * @author 蔡伟明 <314013107@qq.com>
+ */
+class MyInfo extends Validate
+{
+   /* protected $rule = [
+        'name'  =>  'require|max:25',
+        'email' =>  'email',
+    ];*/
+
+
+    protected  $rule = [
+           'nickname'  => 'require|max:25',      // 昵称
+            'autograph'  => 'require|max:50',      //个性签名
+            'real_name'  => 'require|max:10',      //姓名
+           'address'=>'require',          //地址
+           'qq'=>'require|number|length:4,11',    //qq
+            'height'=>'require|number|between:50,300', //身高
+           'measurement'=>'require|max:50', //三围
+           'weight'=>'require|between:30,300', //体重
+           'interest'=>'require',  //爱好
+           'occupation_id'=>'require',  //职业
+           'address'=>'require',  //出没地
+           'birthday'=>'require',  //生日
+       ];
+
+
+    protected    $message = [
+            'nickname.require' => '名称必须',                    // 昵称
+            'nickname.max'     => '名称最多不能超过25个字符',      // 昵称
+            'autograph.require' => '签名必须',                      //个性签名
+            'autograph.max'     => '签名最多不能超过50个字符',     //个性签名
+            'real_name.require' => '姓名必须',                      //姓名
+            'real_name.max'     => '请输入正确姓名',               //姓名
+            'address.require' => '地址必须',                         // 地址
+            'qq.require' => 'qq必须',                                 // qq
+            'qq.number'     => '请输入正确qq',                       //qq
+            'qq.length'     => '请输入正确的qq号',                   //qq
+            'height.require'     => '身高必须',                            //身高
+            'height.number'     => '请输入正确的身高',                   //身高
+            'height.between'     => '请输入正确的身高',                   //身高
+            'measurement.require'     => '三围必须',                            //三围
+            'measurement.max'     => '请输入正确的身高',                   //三围
+            'weight.require'     => '体重必须',                            //体重
+            'weight.between'     => '请输入正确的体重',                        //体重
+            'interest.require'     => '爱好必须',                        //爱好
+            'occupation_id.require'     => '职业必须',                        //职业
+            'address.require'     => '出没地必须',                          //出没地
+            'birthday.require'     => '生日地必须',                          //生日
+        ];
+
+}
+
---
+++ public/static/home/js/user/my_info/head.js
@@ -0,0 +1,66 @@
+var previewrWidth = 110;
+var previewrHeight = 110;
+var $img = $('#imgg');
+$('input[type=file]').change(function() {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        $img.prop('src', e.target.result);
+    }
+    reader.readAsDataURL(this.files[0]);
+    console.log(this.files[0])
+    $('#img').attr('value',$('#imgg').attr('src'));
+    $('#imgg').load(function() {
+        var size = autoSize(this.naturalWidth, this.naturalHeight);
+        $(this).css({
+            width: size.width,
+            height: size.height,
+            top: (previewrHeight - size.height) / 2,
+            left: (previewrWidth - size.width) / 2
+        }).show();
+    });
+});
+
+function autoSize(width, height) {
+    var scale = width / height;
+    if (scale >= previewrWidth / previewrHeight) {
+        height = previewrWidth / scale;
+        width = previewrWidth;
+    } else {
+        width = previewrHeight * scale;
+        height = previewrHeight;
+    }
+    return {
+        width: width,
+        height: height
+    }
+}
+
+$('#form').submit(function () {
+    //
+    if(false){
+        layer.msg('验证失败');
+        return false;
+    }
+    var formElement = document.getElementById("form");
+    var formData = new FormData(formElement);
+    formData.append('',$('#form').serialize());
+    $.ajax({
+        url:'http://'+window.location.host+'/user/my_info/modify',
+        data:formData,
+        dataType:'JSON',
+        type:'post',
+        processData: false,  // 不处理数据
+        contentType: false,  // 不设置内容类型
+        success:function(res){
+            if(res === true){
+                layer.msg('保存成功')
+            }else{
+                layer.msg(res)
+            }
+        },
+        error:function (e) {
+            alert('服务器发生故障')
+        }
+    })
+    return false;
+});
