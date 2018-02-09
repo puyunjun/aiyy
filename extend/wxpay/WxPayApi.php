c102045bdf3acfc1fb53c9563b8896d41f7f19b6
puyunjun <15084341790@163.com>
2018-02-09 17:51:29 +0800

    puyunjun

--- extend/wxpay/WxPayApi.php
+++ extend/wxpay/WxPayApi.php
@@ -546,7 +546,7 @@ class WxPayApi
             curl_setopt($ch, CURLOPT_PROXYPORT, WxPayConfig::CURL_PROXY_PORT);
         }
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);//严格校验
         //设置header
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
